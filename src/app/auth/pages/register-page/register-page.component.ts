a73947aa3144930de0474ccd7db33c52307ff5af
annruiblaz <anruiblaz@gmail.com>
2025-01-12 23:03:17 +0100

    ejemplo de validación super simple en register form

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators.helpers';
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class RegisterPageComponent {
+  public registerForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required, cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
 
+  })
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  isInvalidField() {
+
+  }
+
+  onSubmit() {
+    this.registerForm.markAllAsTouched();
+  }
 }
