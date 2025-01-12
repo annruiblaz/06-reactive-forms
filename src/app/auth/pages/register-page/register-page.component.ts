388680488caa042cc7fd4762ce41a89422e91cf4
annruiblaz <anruiblaz@gmail.com>
2025-01-12 23:12:41 +0100

    validaciones custom de patrones para email y name

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { cantBeStrider } from '../../../shared/validators/validators.helpers';
+import * as customValidators from '../../../shared/validators/validators.helpers';
 
 @Component({
   selector: 'app-register-page',
@@ -9,9 +9,9 @@ import { cantBeStrider } from '../../../shared/validators/validators.helpers';
 })
 export class RegisterPageComponent {
   public registerForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required, cantBeStrider]],
+    name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
+    username: ['', [Validators.required, customValidators.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
 
